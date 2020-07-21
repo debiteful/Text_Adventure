cf340c8f394e1f864f76baddf22c8f654c2517d2
Merthew <timjpatt@gmail.com>
2020-07-20 20:00:28 -0400

    update

--- filereader.js
+++ filereader.js
@@ -26,21 +26,40 @@ function handleFileLoad(event){
 		* startupMessage 	= 	<T><T>
 		* optionText 		= 	<"0"><"0"> where 0 is the index of the entry.
 		* optionAddition 	= 	<'0'><'0'> where 0 is the index of the entry.
-		* optionGroups		=	:0:1,2:0: where 0 is the index of the group, and 1,2 is the list of the entries separated by commas
+		* optionGroups		=	<:0:>1,2<:0:> where 0 is the index of the group, and 1,2 is the list of the entries separated by commas
 		* ===========================================================
 		* Overrides are done by the last conflicting thing in the file being chosen.
 		* Important sections of the text are denoted by <!><!> and will show up as blue text.
 		*/
 		var buffer = raw;
-		buffer = buffer.substring(buffer.search("<T>"));
-		buffer = buffer.substring(3);
-		buffer = buffer.substring(0, buffer.search("<T>"));
-		console.log(buffer);
-		buffer = buffer.replace(/<!>/g, "<a class=\"important\">");
-		buffer = buffer.replace(/<\/!>/g, "</a>");
-		console.log(buffer);
-		startupMessage = buffer;
+		//Pulling out the startup message.
+		buffer = buffer.substring(buffer.search("<T>") + 3);
+		startupMessage = buffer.substring(0, buffer.search("<T>"));
+		buffer = buffer.substring(buffer.search("<T>") + 3);
+		buffer = buffer.trim();
+		startupMessage = startupMessage.replace(/<!>/g, "<a class=\"important\">");
+		startupMessage = startupMessage.replace(/<\/!>/g, "</a>");
 		bodyText += startupMessage;
+		
+		//Start segment parting
+		console.log(buffer);
+		//while(buffer != "" || buffer){
+			switch(buffer.charAt(1)){
+				case '\"':
+				console.log(buffer.charAt(1));
+				
+				
+				break;
+				case '\'':
+				console.log(buffer.charAt(1));
+				break;
+				case ':':
+				console.log(buffer.charAt(1));
+				break;
+			}
+		//}
+		
+		
 		
 		var el = document.getElementById("gameSelectionWrapper");
 		var opacity = 1.0;
